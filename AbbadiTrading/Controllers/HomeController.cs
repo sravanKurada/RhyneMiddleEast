4b03690c57740bd31e145d43aee84d282954dcc2
Kayvan Desktop <k1karim@gmail.com>
2018-01-08 10:44:59 +0400

    Add project files.

---
+++ AbbadiTrading/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AbbadiTrading.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.CurrentPage = "Home";
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            ViewBag.CurrentPage = "AboutUs";
+            return View();
+        }
+    }
+}
---
+++ AbbadiTrading/Controllers/ProductsController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AbbadiTrading.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //
+        // GET: /Products/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Bitumen()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+        public ActionResult Polymers()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+
+        #region Bitumen
+        public ActionResult PenetrationBitumen()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+
+        public ActionResult CutbackBitumen()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+
+        public ActionResult OxidizedBitumen()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+
+        public ActionResult Gilsonite()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+        #endregion
+
+        #region Dates
+
+        public ActionResult Dates()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+        public ActionResult MazafatiDates()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+        public ActionResult PiaromDates()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+        public ActionResult SayerDates()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+        public ActionResult ZahediDates()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+        public ActionResult KabkabDates()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+        public ActionResult DatesSyrup()
+        {
+            ViewBag.CurrentPage = "Product";
+            return View();
+        }
+        #endregion
+
+      
+    }
+}
